8890a8c0c245866ac35e74a90000cae52f28cf90
jasondbroomes-dev <jason.broomes@pursuit.org>
2025-06-17 17:41:06 -0400

    Add beach-story.py script to share a fun beach experience

---
+++ beach-story.py
@@ -0,0 +1,5 @@
+# A little story about going to the beach
+
+print("I love the beach.")
+print("When I go to the beach, I always find a spot to fry amazing fish.")
+print("Not only do I fry fish, I also swim in the salty water.")
