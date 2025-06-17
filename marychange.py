6bf7a0edfed9243532f8eda8c5552425abc36250
jasondbroomes-dev <jason.broomes@pursuit.org>
2025-06-17 18:04:46 -0400

    Add marychange.py script to calculate Mary's spending and change

---
+++ marychange.py
@@ -0,0 +1,18 @@
+# Mary's total money
+mary_money = 50
+
+# Item prices
+apples = 10         # 10 apples cost $10
+limes = 3           # 5 limes cost $3
+fish = 20
+bread = 4
+
+# Total cost
+total_cost = apples + limes + fish + bread
+
+# Calculate change
+change = mary_money - total_cost
+
+# Print results
+print("Mary spent $" + str(total_cost))
+print("Mary has $" + str(change) + " left as change.")
